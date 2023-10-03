92216baf6c1407d12caec79240ca4895a6f23a1c
Veysitasci <veysi7074@gmail.com>
2023-10-03 18:28:46 +0300

    Update callback_handler.py

--- plugins/callback_handler.py
+++ plugins/callback_handler.py
@@ -18,16 +18,16 @@ from webshotbot import WebshotBot
 
 @WebshotBot.on_callback_query(filters.create(lambda _, __, c: c.data == "render"))  # type: ignore
 async def primary_cb(client: WebshotBot, callback_query: CallbackQuery):
-    await callback_query.answer("processing your request")
-    message = await callback_query.message.edit("**processing...**")
+    await callback_query.answer("İsteğinizi işleme alma.\Processing your request.")
+    message = await callback_query.message.edit("**İşleniyor...**\Processing...")
     printer = Printer.from_message(callback_query.message)
     printer.allocate_folder(callback_query.message.chat.id, callback_query.message.id)
-    await message.edit("**please wait you are in a queue...**")
+    await message.edit("**Lütfen bekleyin, sıra sizde...\Please wait you are in a queue...**")
     try:
         future, wait_event = client.new_request(printer, callback_query.message.chat.id)
         await wait_event.wait()
         await message.edit(
-            "**rendering the website...**",
+            "**Web sitesini oluşturma...\Rendering the website...**",
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("render now", "release")]])
             if printer.scroll_control == ScrollMode.MANUAL
             else None,  # type: ignore
@@ -45,7 +45,7 @@ async def primary_cb(client: WebshotBot, callback_query: CallbackQuery):
         await message.edit(f"`{e}`")
         printer.cleanup()
         return
-    await message.edit("**uploading...**")
+    await message.edit("**Yükleniyor/Loading...**")
     if printer.split and printer.fullpage:
         loc_of_images = await asyncio.get_event_loop().run_in_executor(None, split_image, printer.file)
         for media_group in mediagroup_gen(loc_of_images):
@@ -65,7 +65,7 @@ async def primary_cb(client: WebshotBot, callback_query: CallbackQuery):
         )
     await asyncio.gather(
         message.delete(),
-        message.reply_text('__Please toggle "Scroll Site" setting if the output has no content.__'),
+        message.reply_text('__Çıktıda içerik yoksa lütfen "Scroll Site" ayarını değiştirin.\__Please toggle "Scroll Site" setting if the output has no content.__'),
     )
     printer.cleanup()
 
@@ -75,10 +75,10 @@ async def release_cb(client: WebshotBot, callback_query: CallbackQuery):
     event = client.get_request(callback_query.message.chat.id)
     if event is not None:
         event.set()
-        await callback_query.answer("rendering now")
+        await callback_query.answer("Şimdi oluşturuluyor...\Rendering now...")
         await callback_query.message.edit_reply_markup()
     else:
-        await callback_query.answer("please wait")
+        await callback_query.answer("Lütfen bekleyin\Please wait")
 
 
 @WebshotBot.on_callback_query(filters.create(lambda _, __, c: c.data == "res"))  # type: ignore
@@ -91,7 +91,7 @@ async def resolution_cb(_, callback_query: CallbackQuery, render_type: RenderTyp
     current_res = current_res.removeprefix(button_title)
     res_to_change = button_title + get_resolution(current_res=current_res, render_type=render_type)
     message.reply_markup.inline_keyboard[4][0] = InlineKeyboardButton(res_to_change, "res")  # type: ignore
-    await message.edit(text="Choose the prefered settings", reply_markup=message.reply_markup)  # type: ignore
+    await message.edit(text="Tercih edilen ayarları seçin.\Choose the prefered settings.", reply_markup=message.reply_markup)  # type: ignore
 
 
 @WebshotBot.on_callback_query(filters.create(lambda _, __, c: c.data == "format"))  # type: ignore
@@ -117,7 +117,7 @@ async def format_cb(_, callback_query: CallbackQuery, render_type: RenderType):
     else:
         format_to_change = "Format - JPEG"
     reply_markup.inline_keyboard[0][0] = InlineKeyboardButton(format_to_change, "format")
-    await message.edit(text="Choose the prefered settings", reply_markup=reply_markup)
+    await message.edit(text="Tercih edilen ayarları seçin.\Choose the prefered settings.", reply_markup=reply_markup)
 
 
 @WebshotBot.on_callback_query(filters.create(lambda _, __, c: c.data == "options"))  # type: ignore
@@ -125,7 +125,7 @@ async def format_cb(_, callback_query: CallbackQuery, render_type: RenderType):
 async def options_cb(_, callback_query: CallbackQuery, render_type: RenderType):
     reply_markup: InlineKeyboardMarkup = callback_query.message.reply_markup  # type: ignore
     toggled = "show" in reply_markup.inline_keyboard[3][0].text
-    await callback_query.answer(f"{'opening' if toggled else'closing'} additional settings")
+    await callback_query.answer(f"{'opening' if toggled else'closing'} ek ayarlar/additional settings")
     if toggled:
         resolution = "Letter" if render_type == RenderType.PDF else "1280x720"
         reply_markup.inline_keyboard.insert(
@@ -142,7 +142,7 @@ async def options_cb(_, callback_query: CallbackQuery, render_type: RenderType):
             reply_markup.inline_keyboard.pop(-3)
     options_to_change = "hide additional options ˄" if toggled else "show additional options ˅"
     reply_markup.inline_keyboard[3][0] = InlineKeyboardButton(options_to_change, "options")
-    await callback_query.message.edit(text="Choose the prefered settings", reply_markup=reply_markup)
+    await callback_query.message.edit(text="Tercih edilen ayarları seçin.\Choose the prefered settings.", reply_markup=reply_markup)
 
 
 @WebshotBot.on_callback_query()  # type: ignore
@@ -159,14 +159,14 @@ async def configurations_cb(_, callback_query: CallbackQuery):
             ("Split - No" if "Yes" in current_boolean.text else "Split - Yes"),
             "splits",
         )
-        await message.edit(text="Choose the prefered settings", reply_markup=reply_markup)
+        await message.edit(text="Tercih edilen ayarları seçin.\Choose the prefered settings.", reply_markup=reply_markup)
 
     elif cb_data == "page":
         current_page = reply_markup.inline_keyboard[1][0]
         reply_markup.inline_keyboard[1][0] = InlineKeyboardButton(
             ("Page - Partial" if "Full" in current_page.text else "Page - Full"), "page"
         )
-        await message.edit(text="Choose the prefered settings", reply_markup=reply_markup)
+        await message.edit(text="Tercih edilen ayarları seçin.\Choose the prefered settings.", reply_markup=reply_markup)
 
     elif cb_data == "scroll":
         current_load = reply_markup.inline_keyboard[2][0]
@@ -177,10 +177,10 @@ async def configurations_cb(_, callback_query: CallbackQuery):
         elif "Manual" in current_load.text:
             text = "No"
         reply_markup.inline_keyboard[2][0] = InlineKeyboardButton(f"Scroll Site - {text}", "scroll")
-        await message.edit(text="Choose the prefered settings", reply_markup=reply_markup)
+        await message.edit(text="Tercih edilen ayarları seçin.\Choose the prefered settings.", reply_markup=reply_markup)
 
     elif cb_data == "cancel":
-        await callback_query.answer("Canceled your request..!")
+        await callback_query.answer("Talebinizi iptal ettim..!\Canceled your request..!")
         await message.delete()
 
     elif cb_data == "about_cb":
